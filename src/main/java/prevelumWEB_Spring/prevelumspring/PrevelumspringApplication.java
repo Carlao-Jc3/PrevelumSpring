dfee56c3536ea88b3bfa4527cd01ea813b17e53b
Carslo Eduardo <eduardocarlos.hg@gmail.com>
2026-08-13 18:45:02 -0300

    Configuração inicial do projeto Spring Boot

---
+++ src/main/java/prevelumWEB_Spring/prevelumspring/PrevelumspringApplication.java
@@ -0,0 +1,13 @@
+package prevelumWEB_Spring.prevelumspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrevelumspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrevelumspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prevelumWEB_Spring/prevelumspring/PrevelumspringApplicationTests.java
@@ -0,0 +1,13 @@
+package prevelumWEB_Spring.prevelumspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrevelumspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
